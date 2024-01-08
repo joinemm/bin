5464ce8a20b87de473a237d282fa0d6f2ef41e29
Joinemm <joonas@rautiola.co>
2024-01-08 18:42:30 +0200

    Restructure flake

--- flake.nix
+++ flake.nix
@@ -12,95 +12,112 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
 
-    # scripts that are needed as dependencies
-
     lock = pkgs.resholve.writeScriptBin "lock" {
       inputs = with pkgs; [
         xsecurelock
       ];
       interpreter = "/bin/sh";
     } (builtins.readFile ./lock);
+
+    color = pkgs.resholve.writeScriptBin "color" {
+      inputs = with pkgs; [
+        coreutils
+        xcolor
+        xclip
+        libnotify
+      ];
+      interpreter = "/bin/sh";
+    } (builtins.readFile ./color);
+
+    buds = pkgs.resholve.writeScriptBin "buds" {
+      inputs = with pkgs; [
+        bluez
+      ];
+      interpreter = "/bin/sh";
+    } (builtins.readFile ./buds);
+
+    dock = pkgs.resholve.writeScriptBin "dock" {
+      inputs = with pkgs; [
+        mons
+      ];
+      keep = {
+        "~/.fehbg" = true;
+      };
+      execer = [
+        "cannot:${pkgs.mons}/bin/mons"
+      ];
+      interpreter = "${pkgs.bash}/bin/bash";
+    } (builtins.readFile ./dock);
+
+    audio-control = pkgs.resholve.writeScriptBin "audio-control" {
+      inputs = with pkgs; [
+        coreutils
+        pulseaudio
+        procps
+        gnugrep
+        gawk
+      ];
+      execer = [
+        "cannot:${pkgs.procps}/bin/pkill"
+      ];
+      interpreter = "/bin/sh";
+    } (builtins.readFile ./audio-control);
+
+    extract = pkgs.resholve.writeScriptBin "extract" {
+      inputs = with pkgs; [
+        coreutils
+        gnutar # tar
+        xz # unlzma, unxz
+        bzip2 # bunzip2
+        unrar-wrapper # unrar
+        gzip # gunzip, uncompress
+        p7zip # 7z
+        cabextract # cabextract
+        unzip
+        gnused
+      ];
+      execer = [
+        "cannot:${pkgs.p7zip}/bin/7z"
+        "cannot:${pkgs.unrar-wrapper}/bin/unrar"
+        "cannot:${pkgs.gzip}/bin/uncompress"
+        "cannot:${pkgs.gzip}/bin/gunzip"
+      ];
+      interpreter = "/bin/sh";
+    } (builtins.readFile ./extract);
+
+    power-menu = pkgs.resholve.writeScriptBin "power-menu" {
+      inputs = with pkgs; [
+        rofi
+        systemd
+        lock
+      ];
+      execer = [
+        "cannot:${pkgs.rofi}/bin/rofi"
+      ];
+      interpreter = "/bin/sh";
+    } (builtins.readFile ./power-menu);
+
+    all = [
+      color
+      buds
+      dock
+      audio-control
+      extract
+      lock
+      power-menu
+    ];
   in {
+    all = all;
     packages.${system} = {
-      color = pkgs.resholve.writeScriptBin "color" {
-        inputs = with pkgs; [
-          coreutils
-          xcolor
-          xclip
-          libnotify
-        ];
-        interpreter = "/bin/sh";
-      } (builtins.readFile ./color);
-
-      buds = pkgs.resholve.writeScriptBin "buds" {
-        inputs = with pkgs; [
-          bluez
-        ];
-        interpreter = "/bin/sh";
-      } (builtins.readFile ./buds);
-
-      dock = pkgs.resholve.writeScriptBin "dock" {
-        inputs = with pkgs; [
-          mons
-        ];
-        keep = {
-          "~/.fehbg" = true;
-        };
-        execer = [
-          "cannot:${pkgs.mons}/bin/mons"
-        ];
-        interpreter = "${pkgs.bash}/bin/bash";
-      } (builtins.readFile ./dock);
-
-      audio-control = pkgs.resholve.writeScriptBin "audio-control" {
-        inputs = with pkgs; [
-          coreutils
-          pulseaudio
-          procps
-          gnugrep
-          gawk
-        ];
-        execer = [
-          "cannot:${pkgs.procps}/bin/pkill"
-        ];
-        interpreter = "/bin/sh";
-      } (builtins.readFile ./audio-control);
-
-      extract = pkgs.resholve.writeScriptBin "extract" {
-        inputs = with pkgs; [
-          coreutils
-          gnutar # tar
-          xz # unlzma, unxz
-          bzip2 # bunzip2
-          unrar-wrapper # unrar
-          gzip # gunzip, uncompress
-          p7zip # 7z
-          cabextract # cabextract
-          unzip
-          gnused
-        ];
-        execer = [
-          "cannot:${pkgs.p7zip}/bin/7z"
-          "cannot:${pkgs.unrar-wrapper}/bin/unrar"
-          "cannot:${pkgs.gzip}/bin/uncompress"
-          "cannot:${pkgs.gzip}/bin/gunzip"
-        ];
-        interpreter = "/bin/sh";
-      } (builtins.readFile ./extract);
-
-      lock = lock;
-
-      power-menu = pkgs.resholve.writeScriptBin "power-menu" {
-        inputs = with pkgs; [
-          rofi
-          systemd
-          lock
-        ];
-        execer = [
-          "cannot:${pkgs.rofi}/bin/rofi"
-        ];
-        interpreter = "/bin/sh";
-      } (builtins.readFile ./power-menu);
+      inherit
+        color
+        buds
+        dock
+        audio-control
+        extract
+        lock
+        power-menu
+        ;
     };
   };
 }
